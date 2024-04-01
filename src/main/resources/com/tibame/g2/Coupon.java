cba6700feaa3ca4b8ccbcabeca726a5ee65fcb8c
ChihYun Wu <wu20603@gmail.com>
2024-04-01 15:30:01 +0800

    test - create Coupon VO

---
+++ src/main/resources/com/tibame/g2/Coupon.java
@@ -0,0 +1,20 @@
+package com.tibame.g2;
+
+public class Coupon {
+	private Integer couponNo;
+	private String couponName;
+	public Integer getCouponNo() {
+		return couponNo;
+	}
+	public void setCouponNo(Integer couponNo) {
+		this.couponNo = couponNo;
+	}
+	public String getCouponName() {
+		return couponName;
+	}
+	public void setCouponName(String couponName) {
+		this.couponName = couponName;
+	}
+	
+
+}
